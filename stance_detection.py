18d1131c677b55c314b37a63bfdd7140dc10f443
xsling <me@xsl.sh>
2024-03-12 18:06:37 -0700

    refactor: stance detection

--- stance_detection.py
+++ stance_detection.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import os
+import argparse
 from tqdm import tqdm
 from dotenv import load_dotenv
 from api import openai
@@ -33,7 +34,30 @@ def update_stance(statements, stances):
 
 
 if __name__ == "__main__":
-    jsonl_files = glob.glob("response/*/*.jsonl")
+    argParser = argparse.ArgumentParser(description="Stance detection with GPT-4.")
+    argParser.add_argument(
+        "-d",
+        "--directory",
+        choices=[
+            os.path.basename(os.path.normpath(folder))
+            for folder in glob.glob("response/*/")
+        ],
+        help="the directory containing the targeting response files. If not specified, all `.jsonl` files under `response` folder will be tested.",
+    )
+    argParser.add_argument(
+        "-s",
+        "--suffix",
+        default="",
+        help="the file name suffix of the new combined file. If not specified, the original file will be directly replaced.",
+    )
+    args = argParser.parse_args()
+    directory = args.directory
+    suffix = args.suffix
+
+    if directory:
+        jsonl_files = glob.glob(f"response/{directory}/*.jsonl")
+    else:
+        jsonl_files = glob.glob("response/*/*.jsonl")
 
     for file_path in tqdm(jsonl_files, desc="Detecting stances"):
         statements = json.loads(open(file_path, "r", encoding="utf-8").read())
@@ -42,6 +66,6 @@ if __name__ == "__main__":
         stances = openai.stance_detection(query)
         updated_statements = update_stance(statements, stances)
 
-        new_file_path = file_path.replace(".jsonl", "_stance.jsonl")
+        new_file_path = file_path.replace(".jsonl", f"{suffix}.jsonl")
         with open(new_file_path, "w", encoding="utf-8") as f:
             json.dump(updated_statements, f, indent=4)
